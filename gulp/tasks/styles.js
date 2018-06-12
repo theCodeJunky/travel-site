a0063580f52c00729e098364a2c765bcf2131b6e
thecodejunky <thecodejunky@gmail.com>
2018-06-11 18:35:43 -0700

    Added CSS Mixin structure and various html images sizes for hero images

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,10 +4,11 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import')
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
         .on('error', function(error) {
             console.log('Error: ' + error.toString());
             this.emit('end');
